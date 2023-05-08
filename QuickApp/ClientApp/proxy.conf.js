2458a4d5830a833cd6ad7ba0d83c5c870478b825
sakthistaddy <132425830+sakthistaddy@users.noreply.github.com>
2023-05-08 15:47:57 +0530

    Update proxy.conf.js

--- QuickApp/ClientApp/proxy.conf.js
+++ QuickApp/ClientApp/proxy.conf.js
@@ -8,7 +8,7 @@
 const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:44350';
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:44350/';
 
 const PROXY_CONFIG = [
   {
